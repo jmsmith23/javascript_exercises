06c3cf32e61bb9189cc5031aff3c42288079acb5
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-10-15 20:50:16 -0500

    finish display likes and array intersection challenges

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,22 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   const combined = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !combined.includes(arr1[i])) {
+//       combined.push(arr1[i]);
+//     }
+//   }
+//   return combined;
+// }
+
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const combined = [];
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      combined.push(num);
+    }
+  }
+  return combined;
+}
 
 module.exports = arrayIntersection;
--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,17 @@
-function displayLikes() {}
+function displayLikes(names) {
+  const allLikes = names.length;
+
+  if (allLikes === 0) {
+    return 'no one likes this';
+  } else if (allLikes === 1) {
+    return `${names[0]} likes this`;
+  } else if (allLikes === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (allLikes === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${allLikes - 2} others like this`;
+  }
+}
 
 module.exports = displayLikes;
