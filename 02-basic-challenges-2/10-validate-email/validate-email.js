55a0d3d1c2312dbb7c674ec50c9cf2188395b9ad
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-10-24 23:01:42 -0500

    complete validate email challenge

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,26 @@
-function validateEmail() {}
+// function validateEmail(email) {
+//   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+//   return emailRegex.test(email);
+// }
+
+function validateEmail(email) {
+  if (email.indexOf('@') === -1) {
+    return false;
+  }
+
+  const [localPart, domain] = email.split('@');
+
+  if (localPart.length === 0 || domain.length < 3) {
+    return false;
+  }
+
+  const domainExt = domain.split('.');
+
+  if (domainExt.length < 2 || domainExt[1] < 2) {
+    return false;
+  }
+  return true;
+}
 
 module.exports = validateEmail;
